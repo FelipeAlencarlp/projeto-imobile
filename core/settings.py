89baa7d1bc77a70b9f6eb3600dccf0717f8fc7aa
Felipe <felipealencar.0014@gmail.com>
2024-02-21 12:46:22 -0300

    Finalização do projeto

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,3 +117,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# --- Messages --- #
+MESSAGE_TAGS = {
+	constants.ERROR: 'alert-danger',
+	constants.WARNING: 'alert-warning',
+	constants.DEBUG: 'alert-danger',
+	constants.SUCCESS: 'alert-success',
+	constants.INFO: 'alert-info',
+}
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-    path('', views.list_location, name='list_location'), 
+    path('', views.list_location, name='list_location'),
+    path('form-client/', views.form_client, name='client-create'),
+    path('form-immobile/', views.form_immobile, name='immobile-create'),
+    path('form-location/<int:id>/', views.form_location, name='location-create'),
+    path('reports/', views.reports, name='reports'),
 ]
